62a298a3c8ddefedc302d181c273d0f627954327
APEREZB26 <aperez26@autonoma.edu.pe>
2022-06-09 21:11:26 -0500

    [UPDATE]interfaces añadidas

--- Codificador.bat
+++ Codificador.bat
@@ -1,7 +1,7 @@
 @echo off
 set /p nombre=Ingrese su nombre:
 echo %nombre%
-cd C:\Users\j_seb\Desktop\BACKEND\face_recognition_and_liveness\face_recognition
+cd C:\Users\Usuario\Desktop\python-web-ia\face_recognition_and_liveness\face_recognition
 python encode_faces.py -i dataset\%nombre% -e encoded_faces.pickle -d cnn
 exit
 
